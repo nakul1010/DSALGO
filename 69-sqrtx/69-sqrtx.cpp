d20f383088d512d5dacdccdf00a80d1d61eb59b1
Nakul Amate <58786560+nakul1010@users.noreply.github.com>
2022-02-16 13:00:55 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (74.59%) - LeetHub

--- 69-sqrtx/69-sqrtx.cpp
+++ 69-sqrtx/69-sqrtx.cpp
@@ -2,6 +2,22 @@ class Solution {
 public:
     int mySqrt(int x) 
     {
-        return sqrt(x);
+        if(x<=1)
+            return x;
+        long long start = 0, end = x-1, ans = 0, prev_mid=-1;
+        while(start<=end)
+        {
+            long long mid = (start+end)/2;
+            if((mid*mid)==x)
+                return mid;
+            if((mid*mid) < x)
+                ans = mid, start = mid + 1;
+            else
+                end = mid - 1;
+            if(mid == prev_mid)
+                break;
+            prev_mid = mid;
+        }
+        return ans;
     }
 };
