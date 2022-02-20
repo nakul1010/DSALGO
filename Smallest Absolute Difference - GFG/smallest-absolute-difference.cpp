c32b56bbb11dd3f8f54281cdf08b237a6dc36dc3
Nakul Amate <58786560+nakul1010@users.noreply.github.com>
2022-02-20 18:14:18 +0530

    Added solution - LeetHub

--- Smallest Absolute Difference - GFG/smallest-absolute-difference.cpp
+++ Smallest Absolute Difference - GFG/smallest-absolute-difference.cpp
@@ -24,40 +24,42 @@ int main() {
 }
 // } Driver Code Ends
 
-
-
-int countPairs(int *a, int n, int mid) {
-    int res = 0;
-    for (int i = 0; i < n; ++i)
-
-        // Upper bound returns pointer to position
-        // of next higher number than a[i]+mid in
-        // a[i..n-1]. We subtract (a + i + 1) from
-        // this position to count
-        res += upper_bound(a + i, a + n, a[i] + mid) - (a + i + 1);
-    return res;
+int count(int arr[], int n, int mid)
+{
+    int no = 0;
+    for(int i=0;i<n-1;i++)
+    {
+                    //   start  end  comparison
+        no += upper_bound(arr+i,arr+n, arr[i]+mid) - (arr+i+1);
+    }
+    return no;
 }
 
-// Returns k-th absolute difference
-int kthDiff(int a[], int n, int k) {
-    // Sort array
-    sort(a, a + n);
-
-    // Minimum absolute difference
-    int low = a[1] - a[0];
-    for (int i = 1; i <= n - 2; ++i) low = min(low, a[i + 1] - a[i]);
-
-    // Maximum absolute difference
-    int high = a[n - 1] - a[0];
-
-    // Do binary search for k-th absolute difference
-    while (low < high) {
-        int mid = (low + high) >> 1;
-        if (countPairs(a, n, mid) < k)
+int kthDiff(int arr[], int n, int k)
+{
+    if(n==1)
+        return 0;
+        
+    int min_diff = INT_MAX, max_diff = INT_MIN;
+    sort(arr,arr+n);
+    
+    for(int i=0;i<n-1;i++) min_diff = min(min_diff, abs(arr[i+1]-arr[i]));
+    
+    max_diff = arr[n-1]-arr[0];
+    
+    int low = min_diff, high = max_diff;
+    
+    while(low<high)
+    {
+        int mid = (low+high)>>1;// divide by 2
+        if(count(arr,n,mid)<k)
+        {
             low = mid + 1;
+        }
         else
+        {
             high = mid;
+        }
     }
-
     return low;
 }
