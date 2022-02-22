ffdd1cc3eea6b4112898e6aaf0f09f093549fcea
Nakul Amate <58786560+nakul1010@users.noreply.github.com>
2022-02-22 12:45:15 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (28.55%) - LeetHub

--- 171-excel-sheet-column-number/171-excel-sheet-column-number.cpp
+++ 171-excel-sheet-column-number/171-excel-sheet-column-number.cpp
@@ -7,9 +7,9 @@ public:
         for( int i=0,j=n-1; i<n; i++,j--)
         {
             if(i==n-1)
-                no = no +  (int(str[i]-'A')+1);
+                no += (int(str[i]-'A')+1);
             else
-                no = no + (pow(26,j) * (int(str[i]-'A')+1));
+                no += (pow(26,j) * (int(str[i]-'A')+1));
         }
         
         return no;
