4b29337ed445bb7031d40f6deadfa26a6bfeef56
Nakul Amate <58786560+nakul1010@users.noreply.github.com>
2022-02-22 12:43:49 +0530

    Time: 4 ms (47.34%), Space: 6.2 MB (13.67%) - LeetHub

---
+++ 171-excel-sheet-column-number/171-excel-sheet-column-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int titleToNumber(string str) 
+    {
+        int no = 0, n = str.size();
+        
+        for( int i=0,j=n-1; i<n; i++,j--)
+        {
+            if(i==n-1)
+                no += (int(str[i]-'A')+1);
+            else
+                no += (pow(26,j) * (int(str[i]-'A')+1));
+        }
+        
+        return no;
+    }
+};
