94c959b4e844997feab5940b9b16585926f6e016
Nakul Amate <58786560+nakul1010@users.noreply.github.com>
2022-02-26 15:02:27 +0530

    Time: 116 ms (45.38%), Space: 18.1 MB (48.35%) - LeetHub

---
+++ 847-shortest-path-visiting-all-nodes/847-shortest-path-visiting-all-nodes.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int shortestPathLength(vector<vector<int>>& graph) 
+    {
+        int n = graph.size();
+        if(n==1)
+            return 0;
+        
+        int all = (1 << n) - 1;//all visitted 16 - 1 = 15 or 1000 - 1 = 1111
+        // cout<<all;//15
+        
+        queue< pair<int, pair<int,int> > >q;// u , dist, mask
+        set<pair<int,int>>vis;
+        
+        for(int i=0; i<n; i++)
+        {
+            int mask = (1<<i);
+            q.push({i,{0,mask}});
+            vis.insert({i,mask});
+        }   
+        
+        while(q.empty()==false)
+        {
+            int vertex = q.front().first;
+            int dist = q.front().second.first;
+            int src_mask = q.front().second.second;
+            q.pop();
+            
+            for(auto node: graph[vertex])
+            {
+                int new_mask =  (src_mask | (1<<node));//0001 | 0010 = 0011
+                if(new_mask == all)
+                    return dist + 1;
+                else if(vis.find({node, new_mask}) != vis.end())
+                    continue;
+                q.push({node,{dist+1,new_mask}});
+                vis.insert({node,new_mask});
+            }
+            
+        }
+        return 1e9;
+    }
+};
