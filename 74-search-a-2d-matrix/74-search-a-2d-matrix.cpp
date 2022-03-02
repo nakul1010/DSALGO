f6b3fa5e19d23e8d8931d1bb2276fec7e7dea428
Nakul Amate <58786560+nakul1010@users.noreply.github.com>
2022-03-02 11:53:39 +0530

    Time: 0 ms (100.00%), Space: 9.3 MB (95.49%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -4,15 +4,26 @@ public:
     {
         int row = arr.size(), col = arr[0].size();
         
-        int get_row = -1;
-        for(int i=0;i<row;i++)
+        int get_row = -1, start = 0, end = row-1, prev_mid=-1,mid;
+        
+        while(start<=end)
         {
-            if(target <= arr[i][col-1])
+            int mid = (start+end)/2;
+            
+            if(target <= arr[mid][col-1])
             {
-                get_row = i;
-                break;
+                get_row = mid;
+                end = mid-1;
             }
+            else
+            {
+                start=mid+1;
+            }
+            if(prev_mid == mid)
+                break;
+            prev_mid = mid;
         }
+        
         if(get_row!=-1)
         {
             for(int i=0;i<col;i++)
@@ -21,7 +32,7 @@ public:
                     return true;
             }
         }
-        
+    
         return false;
     }
 };
