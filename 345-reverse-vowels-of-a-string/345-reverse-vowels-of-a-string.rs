ea3115197a5754bf5ee98629700a46ba241a6d1e
Nakul Amate <58786560+nakul1010@users.noreply.github.com>
2022-03-03 19:13:10 +0530

    Time: 0 ms (100.00%), Space: 2.6 MB (53.85%) - LeetHub

--- 345-reverse-vowels-of-a-string/345-reverse-vowels-of-a-string.rs
+++ 345-reverse-vowels-of-a-string/345-reverse-vowels-of-a-string.rs
@@ -5,34 +5,34 @@ impl Solution
         // return s;
         let mut n = s.len();
         let mut vec:Vec<char> = s.chars().collect();
-        let mut start = 0;
-        let mut high = s.len()-1;
+        let mut start:isize  = 0;
+        let mut high:isize  = (s.len()-1) as isize ;
         
         while start<high
         {
             // println!("- start : {},  high : {}",start,high);
-            while start<high && is_vowel(vec[start])==false
+            while start<high && is_vowel(vec[start as usize])==false
             {
                 start += 1;
             }
-            while start<high && is_vowel(vec[high])==false
+            while start<high && is_vowel(vec[high as usize])==false
             {
                 high -= 1;
             }
             // println!("-- start : {},  high : {}",start,high);
-            if is_vowel(vec[start])==true && is_vowel(vec[high])==true && start != high
+            if is_vowel(vec[start as usize])==true && is_vowel(vec[high as usize])==true && start != high
             {
                 // println!("start : {},  high : {}",start,high);
                 vec.swap(start as usize, high as usize);
             }
             // println!("--- start : {},  high : {}",start,high);
-            if start==high
-            {
-                break;
-            }
+            // if start==high
+            // {
+            //     break;
+            // }
             start += 1;
             high -= 1;
-            println!("---- start : {},  high : {}",start,high);
+            // println!("---- start : {},  high : {}",start,high);
         }
         
         return vec.into_iter().collect();
