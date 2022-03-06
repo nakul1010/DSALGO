16a8ba446f67a1de3eca10a4b867d12ca17a2639
Nakul Amate <58786560+nakul1010@users.noreply.github.com>
2022-03-06 12:51:47 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (82.70%) - LeetHub

---
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+        
+    int countOrders(int n) 
+    {
+        long res = 1, mod = 1000000007;
+        
+        for(int i=1; i<=n ;i++)
+        {
+            res *= i;//n!
+            res %= mod;
+            
+            res *= (2*i - 1);//2n-1 places 
+            res %= mod;
+        }
+        return res %= mod;
+    }
+};
