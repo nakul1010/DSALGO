043a618268b9939c1e51c760aa83fe4e15fe1758
Nakul Amate <58786560+nakul1010@users.noreply.github.com>
2022-03-07 23:54:57 +0530

    Added solution - LeetHub

--- Merge K sorted linked lists - GFG/merge-k-sorted-linked-lists.cpp
+++ Merge K sorted linked lists - GFG/merge-k-sorted-linked-lists.cpp
@@ -45,63 +45,31 @@ struct Node
 
 class Solution{
   public:
-   Node *mergeTwoSortedLinkedLists(Node *head1, Node *head2)
-   {
-    //Write your code here
-      if(head1==NULL && head2==NULL)
-         return head1;
-      if(head1==NULL)
-         return head2;
-      if(head2==NULL)
-         return head1;
-      Node *fh=NULL;
-      Node *ft=NULL;
-      if(head1->data<head2->data){
-          fh=head1;
-          ft=head1;
-          head1=head1->next;
-      }
-      else{
-          fh=head2;
-          ft=head2;
-          head2=head2->next;
-      }
-      while(head1!=NULL && head2!=NULL){
-         if(head1->data<head2->data){
-              ft->next=head1;
-              ft=head1;
-              head1=head1->next;
-         }
-         else{
-            ft->next=head2;
-            ft=head2;
-            head2=head2->next;
-         }     
-      }
-      if(head1!=NULL){
-         ft->next=head1;
-       
-      }
-      else{
-         ft->next=head2;
-      }
-       return fh;
-   }
-    Node * mergeKLists(Node *arr[], int k)
+    struct compare {
+    bool operator()(Node*n1,Node*n2)
     {
-           // Your code here
-           if(k==1){
-               return arr[0];
-           }
-           Node * result1 = mergeTwoSortedLinkedLists(arr[0],arr[1]);
-           
-           if(k==2){
-               return result1;
-           }
-           for(int i=2;i<k;i++){
-               result1 = mergeTwoSortedLinkedLists(result1,arr[i]);
-           }
-           return result1;
+        
+        return n1->data > n2->data;
+    }
+};
+    //Function to merge K sorted linked list.
+    Node * mergeKLists(Node *arr[], int K)
+    {
+        Node *root = NULL,*tail = NULL;
+        priority_queue<Node*,vector<Node*>,compare> pq;
+        for(int i=0;i<K;i++){
+            while(arr[i]){
+                pq.push(arr[i]);
+                arr[i] = arr[i]->next;
+            }
+        }
+        root = pq.top();pq.pop();tail = root;
+        while(!pq.empty()){
+            tail->next = pq.top();pq.pop();
+            tail = tail->next;
+        }
+        tail->next = NULL;
+        return root;
     }
 };
 
