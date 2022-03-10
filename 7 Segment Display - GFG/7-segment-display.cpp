6b28b6cb18ce779484ac6390f1c0c9d4f7fb6765
Nakul Amate <58786560+nakul1010@users.noreply.github.com>
2022-03-10 11:46:19 +0530

    Added solution - LeetHub

--- 7 Segment Display - GFG/7-segment-display.cpp
+++ 7 Segment Display - GFG/7-segment-display.cpp
@@ -8,22 +8,33 @@ class Solution {
     string sevenSegments(string S, int N) 
     {
         S = S.substr(0, N);
-        int segment[10] = {6, 2, 5, 5, 4, 5, 6, 3, 7, 5};
-        int cnt = 0;
-        for(auto i:S){
-            cnt += segment[i-'0'];
+
+        int values[10] = {6,2,5,5,4,5,6,3,7,5};
+        int insert_values[] = {0,1};
+        int n = N;
+        
+        int wt = 0;
+        for(int i=0; i<S.size(); i++)
+        {
+            wt += values[int(S[i]-'0')];
         }
-        string x = "" ;
-        for(int i = N-1; i>=0;i--){
-            for(int j=0; j<10; j++){
-               if(cnt-segment[j]>=2*i && cnt-segment[j]<=7*i){
-                   x += (j+'0');
-                   cnt -= segment[j];
-                   break;
-               }
+        // cout<<"wt : "<<wt<<endl;
+        string str = "";
+        
+        for(int i=N-1 ;i>=0; i--)
+        {
+            for(int j=0; j<10; j++)
+            {
+                if( (wt - values[j] >= 2*i) && (wt - values[j] <= 7*i))
+                {
+                    str += (j+'0');
+                    wt -= values[j];
+                    break;
+                }
             }
         }
-        return x;
+        
+        return str;
     }
 };
 
